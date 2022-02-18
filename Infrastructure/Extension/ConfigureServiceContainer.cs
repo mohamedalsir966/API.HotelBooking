ac369c6cd1124338d2ed4f3d7b442270fe8ea7a3
Mohamed Alsir <mohamed.alsir@halza.com>
2022-02-18 17:44:49 +0400

    faristcommit

---
+++ Infrastructure/Extension/ConfigureServiceContainer.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Extension
+{
+    public static class ConfigureServiceContainer
+    {
+        public static void AddDbContext(this IServiceCollection serviceCollection,
+             IConfiguration configuration, IConfigurationRoot configRoot)
+        {
+            serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+                   options.UseSqlServer(configuration.GetConnectionString("OnionArchConn") ?? configRoot["ConnectionStrings:OnionArchConn"]
+                , b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        }
+        public static void AddSwaggerOpenAPI(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSwaggerGen(setupAction =>
+            {
+
+                setupAction.SwaggerDoc(
+                    "OpenAPISpecification",
+                    new OpenApiInfo()
+                    {
+                        Title = "Onion Architecture WebAPI",
+                        Version = "1",
+                        Description = "Through this API you can access customer details",
+                        Contact = new OpenApiContact()
+                        {
+                            Email = "amit.naik8103@gmail.com",
+                            Name = "Amit Naik",
+                            Url = new Uri("https://amitpnk.github.io/")
+                        },
+                        License = new OpenApiLicense()
+                        {
+                            Name = "MIT License",
+                            Url = new Uri("https://opensource.org/licenses/MIT")
+                        }
+                    });
+
+                setupAction.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    Description = $"Input your Bearer token in this format - Bearer token to access this API",
+                });
+                setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer",
+                            },
+                        }, new List<string>()
+                    },
+                });
+            });
+
+
+
+        }
+        public static void AddScopedServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+        }
+        public static void AddController(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddControllers().AddNewtonsoftJson();
+        }
+
+    }
+}
---
+++ Service/Features/CustomerFeatures/Queries/GetAllCustomerQuery.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Service.Features.CustomerFeatures.Queries
+{
+    //public class GetAllCustomerQuery : IRequest<IEnumerable<Customer>>
+    //{
+    //    public class GetAllCustomerQueryHandler : IRequestHandler<GetAllCustomerQuery, IEnumerable<Customer>>
+    //    {
+    //        private readonly IApplicationDbContext _context;
+    //        public GetAllCustomerQueryHandler(IApplicationDbContext context)
+    //        {
+    //            _context = context;
+    //        }
+    //        public async Task<IEnumerable<Customer>> Handle(GetAllCustomerQuery request, CancellationToken cancellationToken)
+    //        {
+    //            var customerList = await _context.Customers.ToListAsync(cancellationToken: cancellationToken);
+    //            if (customerList == null)
+    //            {
+    //                return null;
+    //            }
+    //            return customerList.AsReadOnly();
+    //        }
+    //    }
+    //}
+}
